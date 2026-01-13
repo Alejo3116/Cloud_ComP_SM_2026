7788435e819bed73053674bd45cf1295f0af2b5d
Alejandro <you@example.com>
2026-01-12 20:00:12 -0500

    Actualizo hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-	return '<h1>Hola Mundo!!!</h1>'
 
-if __name__ == '__main__':
-	app.run(debug=True)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hola desde AWS Flask!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)

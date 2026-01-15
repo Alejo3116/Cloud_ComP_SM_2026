abdb2e9fc80c8c6873120973e48740ab2f8c80bd
Alejo3116 <laruizc@sanmateo.edu.co>
2026-01-15 00:21:00 -0500

    Hello.py

--- Cloud_ComP_SM_2026-main/Flask/hello.py
+++ Cloud_ComP_SM_2026-main/Flask/hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-	return '<h1>Hola Mundo!!!</h1>'
+    return '<h1>Hola Mundo!!!</h1>'
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
